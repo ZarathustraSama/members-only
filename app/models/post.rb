519b9716f4efe1d7093ffce14866d5f520b35f08
ZarathustraSama <mr.gabriel@live.it>
2024-05-27 09:16:23 +0200

    Added login restriction to #new, #create

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
+  before_action :require_login, only: [:new, :create]
+
   validates :body, presence: true
+
   belongs_to :user
+
+  private
+
+  def require_login
+    redirect_to login_path unless current_user.logged_in?
+  end
 end
