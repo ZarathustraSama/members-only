7cebfb73941724950a7b2a3f204992261e44c5ab
ZarathustraSama <mr.gabriel@live.it>
2024-05-27 09:53:35 +0200

    Made #create for Post controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,24 @@ class Post < ApplicationRecord
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to index_post_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def require_login
-    redirect_to login_path unless current_user.logged_in?
+    redirect_to login_path unless user_signed_in?
+  end
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
